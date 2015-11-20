74b6947c9133295576e64007dc3be3142b944141
Leonid Shvechikov <shvechikov@gmail.com>
2015-11-20 07:34:27 +0100

    dev: little config change

--- examples/chatserver/config.py
+++ examples/chatserver/config.py
@@ -1,6 +1,6 @@
 from django.core.exceptions import PermissionDenied
 
 def get_allowed_channels(request, channels):
-    if not request.user.is_authenticated():
-        raise PermissionDenied('Not allowed to subscribe nor to publish on the Websocket!')
-    return set(channels).intersection(['subscribe-user'])
+    if request and request.user.is_authenticated():
+        return set(channels).intersection(['subscribe-user'])
+    raise PermissionDenied('Not allowed to subscribe nor to publish on the Websocket!')
